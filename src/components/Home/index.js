7d916075f3c1d78ce8169ba4bfc45dc6595f3c4a
Nikhil0836 <nikhilbtech836@gmail.com>
2023-09-10 17:00:30 +0000

    Login App tests

---
+++ src/components/Home/index.js
@@ -0,0 +1,41 @@
+// Write your code here
+import {Component} from 'react'
+import Message from '../Message'
+import Login from '../Login'
+import Logout from '../Logout'
+import './index.css'
+
+class Home extends Component {
+  state = {isLogin: false}
+
+  changeStatus = () => {
+    const {isLogin} = this.state
+    this.setState({isLogin: !isLogin})
+  }
+
+  render() {
+    const {isLogin} = this.state
+
+    let messageEle
+    let btnEle
+
+    if (isLogin) {
+      messageEle = <Message content="Welcome User" />
+      btnEle = <Login changeStatus={this.changeStatus} />
+    } else {
+      messageEle = <Message content="Please Login" />
+      btnEle = <Logout changeStatus={this.changeStatus} />
+    }
+
+    return (
+      <div className="bg-login">
+        <div className="login-card">
+          {messageEle}
+          {btnEle}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
